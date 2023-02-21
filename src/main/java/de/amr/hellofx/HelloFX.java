9a89167ec7c96d15dca9258c7ee371dea93ad568
Armin Reichert <armin.reichert@web.de>
2023-02-21 10:05:12 +0100

    refac

--- src/main/java/de/amr/hellofx/HelloFX.java
+++ src/main/java/de/amr/hellofx/HelloFX.java
@@ -5,12 +5,15 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
 
 	public static void main(String[] args) {
+		System.out.println("JavaFX version: %s, runtime version: %s".formatted(System.getProperty("javafx.version"),
+				System.getProperty("javafx.runtime.version")));
 		launch(args);
 	}
 
@@ -18,12 +21,9 @@ public class HelloFX extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		var scene = new Scene(createSceneContent(), 600, 400);
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Hello FX");
+		primaryStage.setTitle("Hello, JavaFX!");
 		primaryStage.show();
-		var version = System.getProperty("javafx.version");
-		var runtimeVersion = System.getProperty("javafx.runtime.version");
-		System.out
-				.println("Application started, JavaFX version: %s, runtime version: %s".formatted(version, runtimeVersion));
+		System.out.println("Application started");
 	}
 
 	@Override
@@ -33,8 +33,8 @@ public class HelloFX extends Application {
 
 	private StackPane createSceneContent() {
 		var content = new StackPane();
-		var text = new Text("Hello FX");
-		text.setFont(Font.font(60));
+		var text = new Text("Hello, JavaFX!");
+		text.setFont(Font.font("Serif", FontWeight.EXTRA_BOLD, 60));
 		text.setFill(Color.RED);
 		content.getChildren().add(text);
 		return content;
